a6d5bf9866d8544a16fd74e47b19cb025aba4a16
NezaBruce <75426019+NezaBruce@users.noreply.github.com>
2022-05-05 08:52:00 +0200

    Update test.js

--- routes/test.js
+++ routes/test.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const Blog = express.Router();
 const {getall,createNew,commentblog,likeblog,getone,updateblog,deleteblog}=require("../controllers/blog");
-import auth from "../middlewares/autha.js";
-import isAdmin from "../middlewares/admina.js";
 const path =require('path');
 const multer = require('multer');
-import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const PATH =  "../public/phots/"
 
